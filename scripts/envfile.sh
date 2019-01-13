c0666417a6f8e0c3a1ea7b9d6347b86129e1b088
Peter Takacs <peter.takacs@koodilab.com>
2019-01-13 20:42:22 +0100

    Enhanced the scripts

--- scripts/envfile.sh
+++ scripts/envfile.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 cp /var/www/html/.env.example /var/www/html/.env
+chown www-data:www-data /var/www/html/.env
 
 sed -i "s/APP_ENV=.*/APP_ENV=production/" /var/www/html/.env
 sed -i "s/APP_DEBUG=.*/APP_DEBUG=false/" /var/www/html/.env
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,11 +2,14 @@
 
 php /var/www/html/artisan passport:keys
 php /var/www/html/artisan config:clear
-
-while ! mysqladmin ping -h "$DB_HOST" --silent; do
-    sleep 1
-done
-
-php /var/www/html/artisan migrate --force --seed
+php /var/www/html/artisan storage:link
 
 chown -R www-data:www-data /var/www/html
+
+if [ "$DB_HOST" = "mysql" ]; then
+    while ! mysqladmin ping -h "$DB_HOST" --silent; do
+        sleep 1
+    done
+fi
+
+php /var/www/html/artisan migrate --force --seed

9c109f0de12f151298b80cac672903e9e5d4a5b5
Peter Takacs <peter.takacs@koodilab.com>
2019-07-14 20:46:33 +0200

    Changed to laravel websockets

--- scripts/dotenv.sh
+++ scripts/dotenv.sh
@@ -5,6 +5,9 @@ chown www-data:www-data /var/www/html/.env
 
 sed -i "s/APP_ENV=.*/APP_ENV=production/" /var/www/html/.env
 sed -i "s/APP_DEBUG=.*/APP_DEBUG=false/" /var/www/html/.env
+sed -i "s/PUSHER_APP_ID=.*/PUSHER_APP_ID=$(openssl rand -hex 10)/" /var/www/html/.env
+sed -i "s/PUSHER_APP_KEY=.*/PUSHER_APP_KEY=$(openssl rand -hex 10)/" /var/www/html/.env
+sed -i "s/PUSHER_APP_SECRET=.*/PUSHER_APP_SECRET=$(openssl rand -hex 10)/" /var/www/html/.env
 
 php /var/www/html/artisan key:generate
 php /var/www/html/artisan config:clear
